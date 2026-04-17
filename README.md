5b9d454133c5e82611408af89f7119243c1a7498
Arun2198 <arunkv2198@gmail.com>
2026-04-17 19:25:24 +0530

    Updated with all files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MLOps-Production-Ready-Machine-Learning-Project
+
+849279003696.dkr.ecr.us-east-1.amazonaws.com/visa
--- us_visa/components/model_evaluation.py
+++ us_visa/components/model_evaluation.py
@@ -7,6 +7,7 @@ from us_visa.logger import logging
 import sys
 import pandas as pd
 from typing import Optional
+from us_visa.entity.s3_estimator import USvisaEstimator
 from dataclasses import dataclass
 from us_visa.entity.estimator import USvisaModel
 from us_visa.entity.estimator import TargetValueMapping
@@ -30,6 +31,25 @@ class ModelEvaluation:
         except Exception as e:
             raise USvisaException(e, sys) from e
 
+    def get_best_model(self) -> Optional[USvisaEstimator]:
+        """
+        Method Name :   get_best_model
+        Description :   This function is used to get model in production
+        
+        Output      :   Returns model object if available in s3 storage
+        On Failure  :   Write an exception log and then raise an exception
+        """
+        try:
+            bucket_name = self.model_eval_config.bucket_name
+            model_path=self.model_eval_config.s3_model_key_path
+            usvisa_estimator = USvisaEstimator(bucket_name=bucket_name,
+                                               model_path=model_path)
+
+            if usvisa_estimator.is_model_present(model_path=model_path):
+                return usvisa_estimator
+            return None
+        except Exception as e:
+            raise  USvisaException(e,sys)
 
     def evaluate_model(self) -> EvaluateModelResponse:
         """
